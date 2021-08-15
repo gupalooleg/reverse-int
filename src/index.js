11afdaa791c7ac42240580326596939e29b049e4
Oleg Gupalo <gupalooleg@gmail.com>
2021-08-15 14:04:06 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return reverseUsingString(n);
+};
+
+function reverseUsingString(n) {
+    return +`${Math.abs(n)}`.split("").reverse().join("");
+}
+
+function reverseUsingNumber(n) {
+    let result = 0;
+
+    n = Math.abs(n);
+    while (n) {
+        result = result * 10 + (n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    return result;
 }
